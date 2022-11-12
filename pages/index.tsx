6dafb084cf53dc0f66f2208b014466842da6fede
Roman Petrashkevich <roman.petrashkevich@skdo.systems>
2022-11-12 13:44:44 +0300

    initial

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Фильтр</title>
+        <meta name='description' content='Тестовое задание от web secret' />
+        <link rel='icon' href='/favicon-32x32.png' />
+        <link rel='icon' href='/favicon-16x16.png' />
+      </Head>
+      Ну привет некст, давай дружить!
+    </>
+  )
+}
