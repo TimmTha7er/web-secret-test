5aa19ea8ea15a0d02948e5498868d77e05be4083
Roman Petrashkevich <roman.petrashkevich@skdo.systems>
2022-11-12 13:59:47 +0300

    feat: add prettier

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,27 @@
 import Head from 'next/head'
 
-export default function Home() {
+const Home = () => {
   return (
     <>
       <Head>
         <title>Фильтр</title>
-        <meta name='description' content='Тестовое задание от web secret' />
-        <link rel='icon' href='/favicon-32x32.png' />
-        <link rel='icon' href='/favicon-16x16.png' />
+        <meta
+          name="description"
+          content="Тестовое задание от web secret"
+        />
+        <link
+          rel="icon"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          href="/favicon-16x16.png"
+        />
       </Head>
-      Ну привет некст, давай дружить!
+
+      <h1>Ну привет некст, давай дружить!</h1>
     </>
   )
 }
+
+export default Home
