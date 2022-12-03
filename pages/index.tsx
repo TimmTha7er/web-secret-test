05779770521636e066cc6274a865abaa20adc099
Roman Petrashkevich <roman.petrashkevich@skdo.systems>
2022-12-03 22:51:17 +0300

    fix(filter page): add SSR

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,52 +1,18 @@
-import { useEffect, useState } from 'react'
-import { useRouter } from 'next/router'
-
-import Head from 'next/head'
 import { Filter } from '@/components/filter'
 import { Products } from '@/components/products'
+import { Head } from '@/components/head'
 import { ProductService } from '@/api/ProductService'
 import { API_URL } from '@/api/config'
 
-const Page = () => {
-  const router = useRouter()
-  const [data, setData] = useState()
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const data = await ProductService.getProducts(
-        `${API_URL}${router.asPath}`
-      )
-
-      setData(data)
-    }
-
-    fetchData()
-  }, [router.asPath])
-
+const Page = ({ data }) => {
   return (
     <>
-      <Head>
-        <title>Фильтр</title>
-        <meta
-          name="description"
-          content="Тестовое задание от web secret"
-        />
-        <link
-          rel="icon"
-          href="/images/favicons/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          href="/images/favicons/favicon-16x16.png"
-        />
-      </Head>
-
+      <Head />
       <div className="container">
         <div className="page">
           {data && (
             <>
               <Filter data={data.filters} />
-
               <Products data={data.products} />
             </>
           )}
@@ -54,6 +20,14 @@ const Page = () => {
       </div>
     </>
   )
+}
+
+export async function getServerSideProps({ resolvedUrl }) {
+  const data = await ProductService.getProducts(`${API_URL}${resolvedUrl}`)
+
+  return {
+    props: { data },
+  }
 }
 
 export default Page
